bccc92bae5ec7f80e636119113d4adbf4250a1a9
samsmith916 <123485878+samsmith916@users.noreply.github.com>
2024-07-31 23:44:49 +0100

    Update __init__.py

--- src/ruptures/detection/__init__.py
+++ src/ruptures/detection/__init__.py
@@ -3,6 +3,6 @@ r"""Search methods."""
 from .binseg import Binseg
 from .bottomup import BottomUp
 from .dynp import Dynp
-from .kernelcpd import KernelCPD
+# from .kernelcpd import KernelCPD
 from .pelt import Pelt
 from .window import Window
